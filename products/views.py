ecdc8fa83d04a3e2057d7314c549789c2f03192e
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-11 07:12:49 +0300

    added def and url index initial

--- products/views.py
+++ products/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 # создание функций/представлений/контроллеров для отображения наших шаблонов
+def index(request):
+    return render(request, 'products/index.html')
