b310fc1caa2e7ac12a2c0f28d1313bac83be7829
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-11 07:58:34 +0300

    added example context on views.py

--- products/views.py
+++ products/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 # создание функций/представлений/контроллеров для отображения наших шаблонов
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'Test title', 
+        'username': 'Sandzhi',
+    }
+    return render(request, 'products/index.html', context)
 
 def products(request):
     return render(request, 'products/products.html')
