4209e709655e544588159cbb5598dad38708f580
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-13 10:57:02 +0300

    added media url and media root for images

--- store/urls.py
+++ store/urls.py
@@ -17,9 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from products import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     path('products/', views.products, name='products'),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
