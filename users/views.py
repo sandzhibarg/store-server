6826e489dfac2bb6e2578fccd47a522cdd2ae604
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-14 10:47:16 +0300

    added profile.html

--- users/views.py
+++ users/views.py
@@ -31,3 +31,7 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
+
+def profile(request):
+    context = {'title': 'Store - Профиль'}
+    return render(request, 'users/profile.html', context)
