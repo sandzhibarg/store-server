8e84c2d92782fc78e61edd351f2bf79526315437
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-14 11:41:12 +0300

    added errors text when registration

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse # просто возвращает строку, где у нас находится адрес
 
 from users.models import User
@@ -26,6 +26,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Поздравляем! Вы успешно зарегистрировались!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
