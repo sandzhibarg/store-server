5795d218f9f305144b07590670f85352690ac6c3
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-15 08:30:03 +0300

    adjust admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from products.models import ProductCategory, Product, Basket
 
 # Register your models here.
-admin.site.register(Product)
 admin.site.register(ProductCategory)
-admin.site.register(Basket)
+
+@admin.register(Product) # указываем с каким именно классом мы будем работать
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('-name',)
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
--- products/models.py
+++ products/models.py
@@ -9,6 +9,10 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=128, unique=True)
     description = models.TextField(null=True, blank=True)
 
+    class Meta:
+        verbose_name = 'категорию'
+        verbose_name_plural = 'категории'
+
     def __str__(self):
         return self.name
 
@@ -20,6 +24,10 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE) # тут параметр on_delete=PROTECT означает что невозможно будет удалить категорию товара, пока все продукты в этой категории не удаляться или не присвоится другая категория
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
 
     # кастомный вывод названия продкута в админ панели
     def __str__(self):
