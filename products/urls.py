13f34e87959342de80fc313619bbaee16be6405a
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-16 07:27:07 +0300

    added get_context_data gor ProductListView

--- products/urls.py
+++ products/urls.py
@@ -5,7 +5,7 @@ app_name = 'products'
 
 urlpatterns = [
   path('', ProductsListView.as_view(), name='index'),
-  # path('category/<int:category_id>/', products, name='category'),
+  path('category/<int:category_id>/', ProductsListView.as_view(), name='category'),
   # path('page/<int:page_number>/', products, name='paginator'),
   path('baskets/add/<int:product_id>/', basket_add, name='basket_add'), # ../products/baskets/add/<product_id>/
   path('baskets/remove/<int:basket_id>/', basket_remove, name='basket_remove'),
--- products/views.py
+++ products/views.py
@@ -22,20 +22,27 @@ class ProductsListView(ListView):
     model = Product
     template_name = 'products/products.html'
 
-def products(request, category_id=None, page_number=1):
-    # используем тернарный оператор (также можем сразу передать в context)
-    products = Product.objects.filter(category__id=category_id) if category_id else Product.objects.all()
-    
-    per_page = 3
-    paginator = Paginator(products, per_page)
-    products_paginator = paginator.page(page_number)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ProductsListView, self).get_context_data() # определяем наш класс
+        context['title'] = 'Store - Каталог' # переопределяем поля в контексте
+        context['categories'] = ProductCategory.objects.all() #
+        return context
 
-    context = {
-        'title': 'Store - Каталог',
-        'products': products_paginator,
-        'categories': ProductCategory.objects.all(),
-    }
-    return render(request, 'products/products.html', context)
+
+# def products(request, category_id=None, page_number=1):
+#     # используем тернарный оператор (также можем сразу передать в context)
+#     products = Product.objects.filter(category__id=category_id) if category_id else Product.objects.all()
+    
+#     per_page = 3
+#     paginator = Paginator(products, per_page)
+#     products_paginator = paginator.page(page_number)
+
+#     context = {
+#         'title': 'Store - Каталог',
+#         'products': products_paginator,
+#         'categories': ProductCategory.objects.all(),
+#     }
+#     return render(request, 'products/products.html', context)
 
 @login_required
 def basket_add(request, product_id):
