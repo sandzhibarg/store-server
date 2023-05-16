9f63089bba05f6cdbf0e391a5c7e637240a9a9cb
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-16 09:11:45 +0300

    added success_url and login_required for UserProfileView

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from users.views import login, logout, UserRegistrationView, UserProfileView
+from django.contrib.auth.decorators import login_required
 
 app_name = 'users'
 
 urlpatterns = [
   path('login/', login, name='login'), # ../users/login/
   path('registration/', UserRegistrationView.as_view(), name='registration'), # ../users/registration/
-  path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'), # ../users/registration/
+  path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'), # ../users/registration/
   path('logout/', logout, name='logout'), # ../users/registration/
 ]
--- users/views.py
+++ users/views.py
@@ -51,6 +51,9 @@ class UserProfileView(UpdateView):
     model = User
     form_class = UserProfileForm
     template_name = 'users/profile.html'
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=(self.object.id,))
     
     def get_context_data(self, **kwargs):
         context = super(UserProfileView, self).get_context_data()
