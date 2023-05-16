83bd22f75548b7edad10bf673ea456ac1b296975
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-16 09:07:58 +0300

    added UserProfileView

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from users.views import login, profile, logout, UserRegistrationView
+from users.views import login, logout, UserRegistrationView, UserProfileView
 
 app_name = 'users'
 
 urlpatterns = [
   path('login/', login, name='login'), # ../users/login/
   path('registration/', UserRegistrationView.as_view(), name='registration'), # ../users/registration/
-  path('profile/', profile, name='profile'), # ../users/registration/
+  path('profile/<int:pk>/', UserProfileView.as_view(), name='profile'), # ../users/registration/
   path('logout/', logout, name='logout'), # ../users/registration/
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse, reverse_lazy # просто возвращает строку, где у нас находится адрес
 from django.contrib.auth.decorators import login_required
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -30,6 +30,11 @@ class UserRegistrationView(CreateView):
     template_name = 'users/registration.html'
     success_url = reverse_lazy('users:login')
 
+    def get_context_data(self, **kwargs):
+        context = super(UserRegistrationView, self).get_context_data()
+        context['title'] = 'Store - Регистрация'
+        return context
+
 # def registration(request):
 #     if request.method == 'POST':
 #         form = UserRegistrationForm(data=request.POST)
@@ -42,24 +47,35 @@ class UserRegistrationView(CreateView):
 #     context = {'form': form}
 #     return render(request, 'users/registration.html', context)
 
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('users:profile'))
-        else:
-            print(form.errors)
-    else:
-        form = UserProfileForm(instance=request.user) # в параметрах указываем данные пользователя, чтобы они отобразились в полях профиля
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    template_name = 'users/profile.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(UserProfileView, self).get_context_data()
+        context['title'] = 'Store - Личный кабинет'
+        context['baskets'] = Basket.objects.filter(user=self.object)
+        return context
 
-    context = {
-        'title': 'Store - Профиль', 
-        'form': form,
-        'baskets': Basket.objects.filter(user=request.user),
-    }
-    return render(request, 'users/profile.html', context)
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect(reverse('users:profile'))
+#         else:
+#             print(form.errors)
+#     else:
+#         form = UserProfileForm(instance=request.user) # в параметрах указываем данные пользователя, чтобы они отобразились в полях профиля
+
+#     context = {
+#         'title': 'Store - Профиль', 
+#         'form': form,
+#         'baskets': Basket.objects.filter(user=request.user),
+#     }
+#     return render(request, 'users/profile.html', context)
 
 def logout(request):
     auth.logout(request)
