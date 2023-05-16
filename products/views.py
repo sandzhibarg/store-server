e11973392d4bf851872fa02c8b53f39773746518
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-16 07:08:42 +0300

    added class-based view IndexView

--- products/views.py
+++ products/views.py
@@ -1,17 +1,21 @@
+from typing import Any, Dict
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.views.generic.base import TemplateView
 
 from products.models import ProductCategory, Product, Basket
 from users.models import User
 
 # Create your views here.
 # создание функций/представлений/контроллеров для отображения наших шаблонов
-def index(request):
-    context = {
-        'title': 'Store', 
-    }
-    return render(request, 'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
 
 def products(request, category_id=None, page_number=1):
     # используем тернарный оператор (также можем сразу передать в context)
--- store/urls.py
+++ store/urls.py
@@ -3,11 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import index
+from products.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', IndexView.as_view(extra_context={'title': 'Store'}), name='index'),
     path('products/', include('products.urls', namespace='products')), # перенаправка на файл urls.py приложения products
     path('users/', include('users.urls', namespace='users')), # перенаправка на файл urls.py приложения users
 ]
