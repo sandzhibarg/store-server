b10df3f596c49724869d89570a26939ed9a3e3d8
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-17 07:34:42 +0300

    added verification model and send email

--- users/models.py
+++ users/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.mail import send_mail
 
 # Create your models here.
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+    is_verified_email = models.BooleanField(default=False)
+
+class EmailVerification(models.Model):
+    code = models.UUIDField(unique=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    expiration = models.DateTimeField()
+
+    def __str__(self):
+        return r'EmailVerifications objects for {self.user.email}'
+    
+    def send_verification_email(self):
+        send_mail(
+            'subject here',
+            'test verification email',
+            'self.user.email',
+            ['to@example.com'],
+            fail_silently=False,
+        )
