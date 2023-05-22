65f5b00c8079d6e3568347582e108e8844947ff1
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-22 08:59:51 +0300

    added orders views

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from orders.models import Orders
 # Register your models here.
+
+@admin.register(Orders)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status')
+    fields = (
+        'id', 'created',
+        ('first_name', 'last_name'),
+        ('email', 'address'),
+        'basket_history', 'status', 'initiator',
+    )
+    readonly_fields = ('id', 'created')
