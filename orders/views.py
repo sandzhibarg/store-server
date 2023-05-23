025c9e16d108054511bedce065efba335cdbed94
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-23 09:13:16 +0300

    added stripe webhook

--- orders/views.py
+++ orders/views.py
@@ -6,6 +6,8 @@ from django.views.generic.edit import CreateView
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy, reverse
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
@@ -34,6 +36,7 @@ class OrderCreateView(CreateView):
                     'quantity': 1,
                 },
             ],
+            metadata = {'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
             cancel_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_canceled'))
@@ -44,3 +47,39 @@ class OrderCreateView(CreateView):
         form.instance.initiator = self.request.user
         return super(OrderCreateView, self).form_valid(form)
 
+@csrf_exempt
+def stripe_webhook_view(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+        payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+        session = stripe.checkout.Session.retrieve(
+        event['data']['object']['id'],
+        expand=['line_items'],
+        )
+
+        line_items = session.line_items
+        # Fulfill the purchase...
+        fulfill_order(line_items)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
+
+def fulfill_order(session):
+    # TODO: fill me in
+    order_id = int(session.metadata.order.id)
+    print("Fulfilling order")
