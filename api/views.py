e154c4bb878025fa7198f02cc466536710ee41c0
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-29 20:14:42 +0300

    adjusted views.py in api app

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser
 
 from products.models import Product
 from products.serializers import ProductSerializer
@@ -7,4 +7,7 @@ from products.serializers import ProductSerializer
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    def get_permissions(self):
+        if self.action in ('create', 'update', 'destroy'):
+            self.permission_classes = (IsAdminUser,)
+        return super(ProductModelViewSet, self).get_permissions()
