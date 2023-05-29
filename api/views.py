67bc8e6fcead14c6cd4265dc12203c89dcc0aae9
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-29 08:25:10 +0300

    added permissions for delete, post, put api

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from products.models import Product
 from products.serializers import ProductSerializer
@@ -6,3 +7,4 @@ from products.serializers import ProductSerializer
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
--- store/settings.py
+++ store/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'django_extensions',
     'rest_framework',
+    'rest_framework.authtoken',
 
     'products',
     'users',
@@ -253,5 +254,8 @@ STRIPE_WEBHOOK_SECRET = env('STRIPE_WEBHOOK_SECRET')
 # Django REST framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 3,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
