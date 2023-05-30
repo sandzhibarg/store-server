3051a240ca87c004827823ce3ea86177e27519d1
sandzhibarg <sandzhi.barg@bk.ru>
2023-05-30 07:31:03 +0300

    added serializers for basket

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,11 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from api.views import ProductModelViewSet
+from api.views import ProductModelViewSet, BasketModelViewSet
 
 router = routers.DefaultRouter()
 router.register(r'products', ProductModelViewSet)
+router.register(r'baskets', BasketModelViewSet)
 
 app_name = 'api'
 
